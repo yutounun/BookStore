e3308519833f9c27d167cf9994154344ca801aed
yutounun <geek.yuto@gmail.com>
2023-04-24 07:46:44 +0900

    Created books model

---
+++ backend/prisma/migrations/20230423223945_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `books` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `author` VARCHAR(191) NOT NULL,
+    `overview` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
