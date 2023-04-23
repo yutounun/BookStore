d40032ed85ded6806bbedc5b5234fcc65a7804c2
yutounun <geek.yuto@gmail.com>
2023-04-24 06:43:47 +0900

    develop simple CRUD TODO APP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [bookNames, setBookNames] = useState([
+    "react for beginner",
+    "Laravel for beginner",
+  ]);
+  const [inputValue, setInputValue] = useState();
+  const [selectedIndex, setSelectedIndex] = useState();
+  const [showsEdit, setShowsEdit] = useState(false);
+
+  const handleSubmit = () => {
+    setBookNames([...bookNames, inputValue]);
+    setInputValue("");
+  };
+  const handleRemove = (index) => {
+    const newBookNames = [...bookNames];
+    newBookNames.splice(index, 1);
+    setBookNames(newBookNames);
+  };
+  const handleEdit = (index) => {
+    setShowsEdit(true);
+    setSelectedIndex(index);
+    setInputValue(bookNames[index]);
+  };
+  const completeEdit = () => {
+    setShowsEdit(false);
+    setInputValue("");
+    bookNames[selectedIndex] = inputValue;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to Book Store!!</h1>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onInput={(e) => setInputValue(e.target.value)}
+        />
+        {showsEdit ? (
+          <button onClick={completeEdit}>complete editing</button>
+        ) : (
+          <button onClick={handleSubmit}>Add Book</button>
+        )}
+      </div>
+      <ul>
+        {bookNames.map((bookName, index) => (
+          <li key={index}>
+            <button onClick={() => handleEdit(index)}>edit</button>
+            <span>{bookName}</span>
+            <span onClick={() => handleRemove(index)}> X</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
