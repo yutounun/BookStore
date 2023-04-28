321adc59b56083d2b2a3fed131b2f357232c3841
yutounun <geek.yuto@gmail.com>
2023-04-28 09:48:28 +0900

    implement delete, put, post, and getById API as well

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -4,10 +4,73 @@ var express = require("express");
 var router = express.Router();
 
 /* GET books listing. */
-router.get("/", async (req, res, next) => {
+router.get("/", async (req, res) => {
   const prisma = new PrismaClient();
-  const allBooks = await prisma.books.findMany();
-  res.json(allBooks);
+  try {
+    const books = await prisma.books.findMany();
+    res.json(books);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/** POST book */
+router.post("/", async (req, res) => {
+  const prisma = new PrismaClient();
+  try {
+    const book = await prisma.books.create({
+      data: req.body,
+    });
+    res.json(book);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/** DELETE book */
+router.delete("/:id", async (req, res) => {
+  const prisma = new PrismaClient();
+  try {
+    const book = await prisma.books.delete({
+      where: {
+        id: parseInt(req.params.id),
+      },
+    });
+    res.json(book);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/** PUT Book */
+router.put("/:id", async (req, res) => {
+  const prisma = new PrismaClient();
+  try {
+    const book = await prisma.books.update({
+      where: {
+        id: parseInt(req.params.id),
+      },
+      data: req.body,
+    });
+    res.json(book);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/** GET BY ID book */
+router.get("/:id", async (req, res) => {
+  const prisma = new PrismaClient();
+  try {
+    const book = await prisma.books.findUnique({
+      where: {
+        id: parseInt(req.params.id),
+      },
+    });
+    res.json(book);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
