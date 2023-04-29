97a16ddd36b1880e43f8f6b6875f5de04b0221e5
yutounun <geek.yuto@gmail.com>
2023-04-29 14:14:04 +0900

    fix UI design using MUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,22 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Button from "@mui/material/Button";
+import EditIcon from "@mui/icons-material/Edit";
+import ImportContactsIcon from "@mui/icons-material/ImportContacts";
+import {
+  Stack,
+  TextField,
+  TableContainer,
+  Table,
+  TableHead,
+  TableCell,
+  TableRow,
+  TableBody,
+  Paper,
+  Typography,
+} from "@mui/material";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 
 function App() {
   const emptyInput = {
@@ -81,52 +97,105 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Welcome to Book Store!!</h1>
-      <div>
-        <span>タイトル</span>
-        <input
-          type="text"
-          value={inputValues.title}
-          onInput={(e) =>
-            setInputValues({ ...inputValues, title: e.target.value })
-          }
-        />
-        <span>著者</span>
-        <input
-          type="text"
-          value={inputValues.author}
-          onInput={(e) =>
-            setInputValues({ ...inputValues, author: e.target.value })
-          }
-        />
-        <span>概要</span>
-        <input
-          type="text"
-          value={inputValues.overview}
-          onInput={(e) =>
-            setInputValues({ ...inputValues, overview: e.target.value })
-          }
-        />
-        {showsEdit}
-        {showsEdit ? (
-          <button onClick={completeEdit}>complete editing</button>
-        ) : (
-          <button onClick={handleSubmit}>Add Book</button>
-        )}
-      </div>
-      <ul>
-        {books.map((book) => (
-          <li key={book.id}>
-            <button onClick={() => handleEdit(book)}>edit</button>
-            <span>{book.title}</span>
-            <span>{book.author}</span>
-            <span>{book.overview}</span>
-            <span onClick={() => handleRemove(book.id)}> X</span>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Stack alignItems="center" mt={3} mb={5}>
+        <Stack direction="row" itemsAlign="center" justifyContent="center">
+          <Typography variant="h3" justifyContent="center" itemAligns="center">
+            Book Store
+          </Typography>
+          <ImportContactsIcon fontSize="large"></ImportContactsIcon>
+        </Stack>
+        <Stack width={"30%"} spacing={5}>
+          <Stack spacing={2}>
+            <span>タイトル</span>
+            <TextField
+              type="text"
+              value={inputValues.title}
+              onInput={(e) =>
+                setInputValues({ ...inputValues, title: e.target.value })
+              }
+            />
+          </Stack>
+          <Stack spacing={2}>
+            <span>著者</span>
+            <TextField
+              type="text"
+              value={inputValues.author}
+              onInput={(e) =>
+                setInputValues({ ...inputValues, author: e.target.value })
+              }
+            />
+          </Stack>
+          <Stack spacing={2}>
+            <span>概要</span>
+            <TextField
+              type="text"
+              value={inputValues.overview}
+              onInput={(e) =>
+                setInputValues({ ...inputValues, overview: e.target.value })
+              }
+            />
+          </Stack>
+          <Stack mt={2} alignItems="center">
+            {showsEdit ? (
+              <Button
+                color={"primary"}
+                variant="contained"
+                onClick={completeEdit}
+              >
+                complete editing
+              </Button>
+            ) : (
+              <Button
+                color={"primary"}
+                variant="contained"
+                onClick={handleSubmit}
+              >
+                Add Book
+              </Button>
+            )}
+          </Stack>
+        </Stack>
+      </Stack>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell>Author</TableCell>
+              <TableCell>OverView</TableCell>
+              <TableCell>CreatedAt</TableCell>
+              <TableCell>Edit</TableCell>
+              <TableCell>Remove</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {books.map((book) => (
+              <TableRow
+                key={book.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {book.title}
+                </TableCell>
+                <TableCell>{book.author}</TableCell>
+                <TableCell>{book.overview}</TableCell>
+                <TableCell>{book.created_at}</TableCell>
+                <TableCell>
+                  <EditIcon onClick={() => handleEdit(book)}>edit</EditIcon>
+                </TableCell>
+                <TableCell>
+                  <RemoveCircleOutlineIcon
+                    onClick={() => handleRemove(book.id)}
+                  ></RemoveCircleOutlineIcon>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
 
