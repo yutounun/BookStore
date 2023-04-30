0630d5b2266083b69ee3e66faac9f7d6fd6529a1
yutounun <geek.yuto@gmail.com>
2023-05-01 07:16:37 +0900

    update allowed origin

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 
 const app = express();
 const corsOptions = {
-  origin: "https://644e6d64ba939e50b8a960b6--relaxed-horse-0f6752.netlify.app/", // 許可したいオリジンを指定
+  origin: "https://relaxed-horse-0f6752.netlify.app/", // 許可したいオリジンを指定
   credentials: true, // レスポンスヘッダーにAccess-Control-Allow-Credentialsを追加。ユーザー認証等を行う場合は、これがないとブラウザがレスポンスを捨ててしまうそう。
   optionsSuccessStatus: 200, // レスポンスのHTTPステータスコードを「200(成功)」に設定
 };
